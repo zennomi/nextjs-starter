693fe0546055a6a8f15c06a27d6909e03fdb7c60
Zennomi <43679352+zennomi@users.noreply.github.com>
2026-03-15 23:03:45 +0700

    refactor: update Next.js configuration to support development and production builds

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
 import bundleAnalyzer from "@next/bundle-analyzer";
 import type { RuleSetRule } from "webpack";
@@ -7,32 +8,44 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true"
 });
 
-const nextConfig: NextConfig = {
-  reactCompiler: true,
-  htmlLimitedBots: /.*/,
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: [
-          {
-            loader: "@svgr/webpack",
-            options: { typescript: true, icon: true, titleProp: true, svgo: true, prettier: false }
-          }
-        ],
-        as: "*.js"
+export default function nextConfig(phase: string): NextConfig {
+  const nextConfig: NextConfig = {
+    distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next/dev" : ".next/prod",
+    reactCompiler: true,
+    htmlLimitedBots: /.*/,
+    turbopack: {
+      rules: {
+        "*.svg": {
+          loaders: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                typescript: true,
+                icon: true,
+                titleProp: true,
+                svgo: true,
+                prettier: false
+              }
+            }
+          ],
+          as: "*.js"
+        }
       }
+    },
+    webpack(config) {
+      const rules = config.module.rules as RuleSetRule[];
+      const fileLoaderRule = rules.find(
+        (rule): rule is RuleSetRule =>
+          !!rule &&
+          typeof rule === "object" &&
+          rule.test instanceof RegExp &&
+          rule.test.test(".svg")
+      );
+      if (fileLoaderRule) fileLoaderRule.exclude = /\.svg$/i;
+      config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
+      return config;
     }
-  },
-  webpack(config) {
-    const rules = config.module.rules as RuleSetRule[];
-    const fileLoaderRule = rules.find(
-      (rule): rule is RuleSetRule =>
-        !!rule && typeof rule === "object" && rule.test instanceof RegExp && rule.test.test(".svg")
-    );
-    if (fileLoaderRule) fileLoaderRule.exclude = /\.svg$/i;
-    config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
-    return config;
-  }
-};
+  };
 
-export default withBundleAnalyzer(nextConfig);
+  return withBundleAnalyzer(nextConfig);
+}
